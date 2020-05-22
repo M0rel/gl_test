9ff2735fa30909db36ab70ea59783a0d88554202
Vadym Kharchuk <vadimharchuk@gmail.com>
2020-05-22 12:07:35 +0300

    fix

---
+++ cables.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+void calculation(const int &d, int n, int &res) {
+    res += d * n;
+    n--;
+    if (n != 0) {
+        calculation(d,n,res);
+    }
+}
+int main (int argc, char const *argv[]) {
+    int N = atoi(argv[1]);
+    int D = atoi(argv[2]);
+    int res = 0;
+    calculation(D,N,res);
+    std::cout << res;
+    return 0;
+}
